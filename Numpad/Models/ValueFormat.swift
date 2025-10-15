d384763e80d56298562ce4f8ad70dc6a737f71da
Benny Wijatno <benny.wijatno@sigfig.com>
2025-10-15 09:15:53 -0700

    Fix import errors for UIKit and SwiftData

--- Numpad/Models/ValueFormat.swift
+++ Numpad/Models/ValueFormat.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 enum ValueFormat: String, Codable, CaseIterable, Identifiable {
     case integer
--- Numpad/Views/AddEntryView.swift
+++ Numpad/Views/AddEntryView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct AddEntryView: View {
     @Environment(\.dismiss) private var dismiss
